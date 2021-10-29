241ecb763bb1a9844a7b59d1488abe8b27567a7a
Prissy <89450637+prissyyy@users.noreply.github.com>
2021-10-29 14:19:50 +0700

    Add files via upload

--- 05_Decision-Tree&Random-Forest.R
+++ 05_Decision-Tree&Random-Forest.R
@@ -2,72 +2,72 @@
 install.packages("party")
 
 ##Decision Tree
-#Memanggil data
-data("bodyfat", package="TH.data")
-bodyfat
-str(bodyfat)
-summary(bodyfat)
+#Memanggil data di R Data Sets
+data(iris)
+iris
+str(iris)
+summary(iris)
 
 #Separate training and test datasets
 set.seed(123)
-ind <- sample(2, nrow(bodyfat), 
-              replace=TRUE, prob=c(0.8, 0.2))
+ind <- sample(2, nrow(iris), 
+              replace=TRUE, prob=c(0.7, 0.3))
 ind
 
-trainData <- bodyfat[ind==1,]
+trainData <- iris[ind==1,]
 trainData
-testData <- bodyfat[ind==2,]
+testData <- iris[ind==2,]
 testData
 
 #MODEL
 library(party)
-myFormula <- DEXfat ~ age + waistcirc + hipcirc + elbowbreadth + kneebreadth + anthro3a + anthro3b + anthro3c + anthro4
+myFormula <- Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width
 myFormula
 
 #cek prediksi model
-bodyfat_ctree <- ctree(myFormula, data=trainData)
-bodyfat_ctree
-table(predict(bodyfat_ctree), trainData$DEXfat)
+iris_ctree <- ctree(myFormula, data=trainData)
+iris_ctree
+table(predict(iris_ctree), trainData$Species)
 
 #buat alur pohon
-plot(bodyfat_ctree)
+plot(iris_ctree)
 
-testPred <- predict(bodyfat_ctree, newdata = testData)
+testPred <- predict(iris_ctree, newdata = testData)
 testPred
-table(testPred, testData$DEXfat)
+table(testPred, testData$Species)
 
 #Cek akurasi
 accuracy <- sum(diag(testPred))
-accuracy
 print(paste('Accuracy for test',accuracy))
 
 ##Random Forest
 install.packages("randomForest")
 
-#Data testing dan training
-ind <- sample(2, nrow(bodyfat), 
+#Membagi data menjadi data testing & training
+ind <- sample(2, nrow(iris), 
               replace=TRUE, 
               prob=c(0.7, 0.3))
 ind
 
-training <- bodyfat[ind==1,]
+training <- iris[ind==1,]
 training
-testing <- bodyfat[ind==2,]
+testing <- iris[ind==2,]
 testing
 
 #Membuat model prediksi
 library(randomForest)
 rf <- randomForest(
-  DEXfat ~ .,
+  Species ~ .,
   data=training
 )
 rf
 
-table(predict(rf), training$DEXfat)
+table(predict(rf), training$Species)
+print(rf)
 
-#Prediksi Model dengan data testing
+#Menguji Model dengan data testing
 Prediksi <- predict(rf, newdata=testing)
-table(Prediksi, testing$DEXfat)
+table(Prediksi, testing$Species)
 
 #Akurasi
 accuracyR <- sum(diag(Prediksi))
